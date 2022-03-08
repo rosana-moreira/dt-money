f637b8f5bf3ae73ead6637a33d20622d098a1395
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-03-08 19:08:24 -0300

    criaçao do global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <GlobalStyle/>
     </div>
   );
 }
